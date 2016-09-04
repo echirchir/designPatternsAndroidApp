fff2a6f22824843fef4ff6c70c7bc5b45b80b435
Elisha <elisha.java@gmail.com>
2016-09-04 15:26:51 -0500

    refactoring fragment name

--- app/src/main/java/com/simpledeveloper/designpatternspro/CoreDPPActivity.java
+++ app/src/main/java/com/simpledeveloper/designpatternspro/CoreDPPActivity.java
@@ -84,22 +84,22 @@ public class CoreDPPActivity extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class DesignPatternInfoFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
+        public DesignPatternInfoFragment() {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static DesignPatternInfoFragment newInstance(int sectionNumber) {
+            DesignPatternInfoFragment fragment = new DesignPatternInfoFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
@@ -129,8 +129,8 @@ public class CoreDPPActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // Return a DesignPatternInfoFragment (defined as a static inner class below).
+            return DesignPatternInfoFragment.newInstance(position + 1);
         }
 
         @Override
